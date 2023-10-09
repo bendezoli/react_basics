bc5b335879ec117908ba12e69518991fc4653320
bendezoli <zoltan@octopus-digital.pro>
2023-10-09 22:42:06 +0300

    useState added

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -1,14 +1,22 @@
+import { useState } from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import PropTypes from "prop-types";
 const ExpenseItem = ({ data }) => {
+  const [title, setTitle] = useState(data.title);
+
+  const updateTitle = () => {
+    setTitle("Title updated");
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={data.date} />
       <div className="expense-item__description">
-        <h2>{data.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${data.amount}</div>
+        <button onClick={updateTitle}>Change date</button>
       </div>
     </Card>
   );
