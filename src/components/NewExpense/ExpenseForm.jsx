42d877e0ca82ce85b64499d5127d1ebc87d19c48
bendezoli <zoltan@octopus-digital.pro>
2023-10-11 12:19:12 +0300

    validity added

--- src/components/NewExpense/ExpenseForm.jsx
+++ src/components/NewExpense/ExpenseForm.jsx
@@ -6,6 +6,8 @@ const ExpenseForm = () => {
   const [enteredAmount, setEnteredAmount] = useState("");
   const [enteredDate, setEnteredDate] = useState("");
 
+  const [validity, setValidity] = useState("");
+
   // with one state
   // call: userInput.enteredTitle
   // u have to update the all values otherwise the data will be lost
@@ -16,7 +18,13 @@ const ExpenseForm = () => {
   // });
 
   const titleChangeHandler = (event) => {
-    // setEnteredTitle(event.target.value);
+    setEnteredTitle(event.target.value);
+
+    if (event.target.value.length > 3) {
+      setValidity("Valid field");
+    } else {
+      setValidity("Invalid filed");
+    }
     // setUserInput({
     // ...userInput,
     //   enteredTitle: event.target.value,
@@ -46,6 +54,7 @@ const ExpenseForm = () => {
           <div className="new-expense__control">
             <label htmlFor="">Title</label>
             <input type="text" onChange={titleChangeHandler} />
+            <p>{validity}</p>
           </div>
 
           <div className="new-expense__control">
