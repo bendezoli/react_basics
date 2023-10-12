8e4e841391bb471c68adc619cad4b24ccdedc576
bendezoli <zoltan@octopus-digital.pro>
2023-10-12 17:26:48 +0300

    some html optimalization

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -4,14 +4,16 @@ import Card from "../UI/Card";
 import PropTypes from "prop-types";
 const ExpenseItem = ({ data }) => {
   return (
-    <Card className="expense-item">
-      {/* <pre>{JSON.stringify(data, null, 2)}</pre> */}
-      <ExpenseDate date={data.date} />
-      <div className="expense-item__description">
-        <h2>{data.title}</h2>
-        <div className="expense-item__price">${data.amount}</div>
-      </div>
-    </Card>
+    <li>
+      <Card className="expense-item">
+        {/* <pre>{JSON.stringify(data, null, 2)}</pre> */}
+        <ExpenseDate date={data.date} />
+        <div className="expense-item__description">
+          <h2>{data.title}</h2>
+          <div className="expense-item__price">${data.amount}</div>
+        </div>
+      </Card>
+    </li>
   );
 };
 
