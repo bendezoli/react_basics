b1ba870ac6d15c647d7cc5532a3aa9dd4a86cc5e
bendezoli <zoltan@octopus-digital.pro>
2023-10-16 09:53:08 +0300

    chart added without functionality

--- src/components/Charts/Chart.jsx
+++ src/components/Charts/Chart.jsx
@@ -1,7 +1,18 @@
 import ChartBar from "./ChartBar";
-import "./ChartBar.css";
-const Chart = () => {
-  return <div className="chart"></div>;
+import "./Chart.css";
+const Chart = (props) => {
+  return (
+    <div className="chart">
+      {props.dataPoints.map((dataPoint, dataPointINdex) => (
+        <ChartBar
+          value={dataPoint.value}
+          maxValue={null}
+          label={dataPoint.label}
+          key={dataPointINdex}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default Chart;
---
+++ src/components/Expenses/ExpensesChart.jsx
@@ -0,0 +1,20 @@
+import Chart from "../Charts/Chart";
+
+const ExpensesChart = (props) => {
+  const chartDatapoints = [
+    { label: "Jan", value: 0 },
+    { label: "Feb", value: 0 },
+    { label: "MArch", value: 0 },
+    { label: "April", value: 0 },
+    { label: "May", value: 0 },
+    { label: "June", value: 0 },
+    { label: "July", value: 0 },
+    { label: "Aug", value: 0 },
+    { label: "Sept", value: 0 },
+    { label: "Oct", value: 0 },
+    { label: "Nov", value: 0 },
+    { label: "Dec", value: 0 },
+  ];
+  return <Chart />;
+};
+export default ExpensesChart;
