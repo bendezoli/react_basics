19547cff1551fe6f485200a3e2d634ae748dd493
bendezoli <zoltan@octopus-digital.pro>
2023-10-17 13:45:37 +0300

    module teleported outside in the root

--- src/components/UI/ErrorModal.jsx
+++ src/components/UI/ErrorModal.jsx
@@ -1,22 +1,39 @@
 import React from "react";
+import ReactDom from "react-dom";
+import "./ErrorModalmodule.css";
 
-import classes from "../UI/ErrorModalmodule.css";
+const BackDrop = () => {
+  return <div className={"backdrop"} />;
+};
+
+const Modal = (props) => {
+  return (
+    <div className={"modal"}>
+      <header className={"header"}>
+        <h2>title</h2>
+      </header>
+      <div className={"content"}>
+        <p>error message</p>
+      </div>
+      <footer className={"actions"}>
+        <button onClick={props.closeModal}>Okay</button>
+      </footer>
+    </div>
+  );
+};
 
 const ErrorModal = (props) => {
   return (
     <React.Fragment>
-      <div className={"backdrop"} />
-      <div className={"modal"}>
-        <header className={"header"}>
-          <h2>title</h2>
-        </header>
-        <div className={"content"}>
-          <p>error message</p>
-        </div>
-        <footer className={"actions"}>
-          <button onClick={props.closeModal}>Okay</button>
-        </footer>
-      </div>
+      {ReactDom.createPortal(
+        <BackDrop />,
+        document.getElementById("backdrop-root")
+      )}
+
+      {ReactDom.createPortal(
+        <Modal closeModal={props.closeModal} />,
+        document.getElementById("overlay-root")
+      )}
     </React.Fragment>
   );
 };
